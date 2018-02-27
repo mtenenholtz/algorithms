b431aab20889e20af9fa57d0389a34c57cf23c46
mtenenholtz <marktenenholtz@gmail.com>
2018-02-27 17:30:59 -0500

    Added simpler solution in comment

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -8,3 +8,12 @@ def palindrome(str):
     rev_str = [x for x in reversed(str)]
     result = map(lambda x: x[0] == x[1], zip(str, rev_str))
     return all(result)
+
+
+"""
+Much easier solution (wanted to practice built-ins above):
+
+def palindrome(str):
+    rev_str = [x for x in reversed(str)]
+    return str == rev_str
+"""
