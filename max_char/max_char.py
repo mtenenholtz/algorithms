864c220b6a5a5e820e0d04502a160dcbbee175cf
mtenenholtz <marktenenholtz@gmail.com>
2018-02-27 18:51:32 -0500

    Style cleanup

--- max_char/max_char.py
+++ max_char/max_char.py
@@ -6,8 +6,8 @@ Directions:
 
 def max_char(str):
     char_counts = {}
-    max_char = str[0]
     max_count = 0
+    max_char = str[0]
 
     for char in str:
         char_counts[char] = char_counts.get(char, 0) + 1
